2aa137c6e5784511517dd04417ceb3b343662993
Yong Han <yonghunhan@gmail.com>
2017-09-29 17:16:44 -0400

    several files added

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'T441IgE2XxUAyVBLrp1vcWe9M',
+  consumer_secret: 'u7hHNGbAwsd4TfYlT6BVGOZGiHijx77fkfxISmYe53lbAAsLZR',
+  access_token_key: '913871201222656000-L0lZy8mM39075GHBOV7NXYx82ehGm6e',
+  access_token_secret: 'ZzVUFSvMF1E7kiVJacBu6dJGSl9gCpfPf2pX8NBq8aeGx',
+}
+
+module.exports = twitterKeys;
